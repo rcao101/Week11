3960b2f2a5ac4efcddea980a420a3a61bf8786a1
ruby <rubycao04@gmail.com>
2022-11-15 09:44:41 -0800

    tried three different function methodds

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+// function greet(username) {
+
+//     console.log(`Hello ${username}`);
+// }
+
+// greet("Ruby");
+
+// let greet = function (username) {
+//     console.log(`Hello ${username}`);
+// }
+
+// greet("Ruby");
+
+let greet = (username) => {
+    console.log(`Shalom ${username}`);
+}
+
+greet("Ruby");
