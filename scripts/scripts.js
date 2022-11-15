a82e12291d2ceabc731a8248024abe941c96c8d2
ruby <rubycao04@gmail.com>
2022-11-15 11:43:08 -0800

    eeryone fixed it thanks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,41 +18,70 @@
 // const result = greet("ruby", "night");
 // console.log(result)
 
-const unorderedlistelement = document.querySelector(".shopping") //dom element
+let greet = function(userName){
+    console.log("YOU GOT " + userName);
+}
 
-function populatelist (list) 
+greet("MAIL");
 
-{
-    for (let i = 0; i <list.length ; i++) 
+let hello = (nameUser) => {
+    console.log("WELCOME " + nameUser);
+}
 
+hello("eRIC")
+
+function name_name(str,time = "Day"){
+    return(`Hallo ${time} ${str}`);
+}
+
+const results = name_name("guy", "day");
+console.log(results);
+
+let shoppingList = ["chesse", "bread", "ham", "green cheese"]
+
+const ul_list = document.querySelector(".shopping"); 
+function populate(list){
+    // alist.forEach(item){
+    // item = `<li>${item}</li>`,
+    // console. log (item);
+    // }
+    for (let i = 0; i < list.length; i++)
     {
-        console.log(list[i]) //for each of these items, create an li, append it to unorderedelement
-        const listitemElement = document.createElement("li");
-        listitemElement.textContent = list[i]
-        unorderedlistelement.appendChild(listitemElement);
-    } 
-        
+        // console.log(list[i]);
+        const ListItemElement = document.createElement("li");
+        ListItemElement.textContent = list[i];
+        ul_list.append(ListItemElement);
+
+    }
+}
+populate(shoppingList)
+
+function changeStyle(){
+    ul_list.classList.add("squareList");
+    ul_list.classList.remove("circleList");
+
 }
 
-function squarebull (list)
-{
-    unorderedlistelement.classList.remove("circleList");
-    unorderedlistelement.classList.add ("squarelist");
+changeStyle()
 
+const images = document.querySelector("#shoppingCar")
+function updateImage(){
+    images.src = "https://duet-cdn.vox-cdn.com/thumbor/0x0:2040x1351/2048x1365/filters:focal(1020x676:1021x677):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/24195455/226406_Nvidia_RTX_4080_Review_TWarren_0001.jpg"
+    images.width = 600;
+    images.height = 300;
+    images.alt = "My dream"
 }
+updateImage();
 
-function updateImage()
-{
-    const imageElement = document.querySelector("#shoppingCart")
-    console.log(imageElement)
-    imageElement.src = "https://cdn-icons-png.flaticon.com/512/263/263142.png"
-    imageElement.width = 100;
-    imageElement.height = 100;
-    imageElement.alt = "Shopping cart icon"
+const newList = document.querySelectorAll(".shopping li")
+function goGreen(){
+
+    for (let i = 0; i < newList.length; i++)
+    {
+        if(newList[i].textContent.includes("green")){
+            newList[i].classList.add("greenText");
+        }
+    }
 }
-
-
-let shoppinglist = ["ham", "cheese", "bread"];
-
-populatelist(shoppinglist);
+goGreen()
 
